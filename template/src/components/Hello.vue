a3a669bbc4ccc18637e552876916516bc08ecad1
williamlogan <william.logan@mobilecapitalgroup.com>
2017-03-07 11:29:01 -0600

    Template tab errors

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -10,25 +10,25 @@
   {{/store}}
   export default {
     data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
-    {{#if store}}
-    return {}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    {{else}}
-    return {
-      msg: 'Hello Vue!'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    {{/if}}
-  }{{#if_eq lintConfig "airbnb"}}{{#unless store}},{{/unless}}{{/if_eq}}{{#store}},{{/store}}
-  {{#store}}
-  computed: mapState({
-    msg: state => state.Hello.message{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/store}}
+      {{#if store}}
+      return {}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+      {{else}}
+      return {
+        msg: 'Hello Vue!'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+      }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+      {{/if}}
+    }{{#if_eq lintConfig "airbnb"}}{{#unless store}},{{/unless}}{{/if_eq}}{{#store}},{{/store}}
+    {{#store}}
+    computed: mapState({
+      msg: state => state.Hello.message{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    }){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    {{/store}}
   }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1 {
-    color: #42b983;
-  }
+h1 {
+color: #42b983;
+}
 </style>
